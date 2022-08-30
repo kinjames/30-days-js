b3d26398bc950376d97c29a3c1df2637eb1d14a9
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-08-30 11:55:42 +0000

    final update

---
+++ day01/script.js
@@ -0,0 +1,55 @@
+//Exercies
+
+//1.Write a single line comment which says, comments can make code readable.
+//comments can make code readable.
+
+//2.Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript.
+
+//3.Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+ comments can make code readable, easy to reuse and informative
+*/
+
+//4.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+ let num = 1
+ let str = 'Hello, World!';
+ let bool = true;
+ let und;
+ let empty =null;
+
+ //5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+ console.log(typeof(num), num);
+ console.log(typeof(str), str);
+ console.log(typeof(bool), bool);
+ console.log(typeof(und), und);
+ console.log(typeof(empty), empty);
+
+ //6.Declare four variables without assigning values
+  let myName;
+  let myAge;
+  let myWork;
+  let isMarried;
+
+//7.Declare four variables with assigned values
+let myName_1 = 'James Kofi Myers';
+let myAge_1 = 24;
+let myWork_1 = "Programmer";
+let isMarried_1 = false;
+
+//8.Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "James";
+let lastName = "Myers";
+let maritalStatus = false;
+let country = 'Ghana';
+let age = 24;
+
+//9.Declare variables to store your first name, last name, marital status, country and age in a single line
+let name_1 = "James", name_2 = "Myers", isMarried_2 = false, myCountry = 'Ghana', hisAge = 24;
+
+//10.Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+personalAge = 24;
+yourAge = 30;
+
+console.log(`I am ${personalAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
