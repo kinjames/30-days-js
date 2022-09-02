ddc67cfb60b6a4f72d03ac3c99cfce8df390440a
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-09-02 22:02:27 +0000

    update

--- day05/script.js
+++ day05/script.js
@@ -25,3 +25,35 @@ console.log(firstItem,middleItem,lastItem);
 //5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
 let mixedDataTypes = [1, true, null, "apple", "banana", false];
 console.log(mixedDataTypes.length);
+
+//6.Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+let itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+//7.Print the array using console.log()
+console.log(itCompanies);
+
+//8.Print the number of companies in the array
+console.log(itCompanies.length);
+
+//9.Print the first company, middle and last company
+let firstCompany = itCompanies[0];
+let middleCompany = itCompanies[3];
+let lastCompany = itCompanies[itCompanies.length - 1];
+
+console.log(firstCompany,middleCompany,lastCompany);
+
+//10.Print out each company
+for(i=0;i<=itCompanies.length-1;i++){
+    console.log(itCompanies[i]);
+}
+
+//11.Change each company name to uppercase one by one and print them out
+for(i=0;i<=itCompanies.length-1;i++){
+    console.log(itCompanies[i].toUpperCase())
+};
+
+//12.Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+console.log(itCompanies.toString());
+
+//13.Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
