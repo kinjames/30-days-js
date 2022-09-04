0a858cea1af022659c7398fe6c9e61ab3cc35dcf
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-09-04 21:14:18 +0000

    update

--- day02/script.js
+++ day02/script.js
@@ -159,7 +159,7 @@ let becauseSentence = 'You cannot end a sentence with because because because is
  //3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
  const sentence1 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
- let reg = /%/gi;
+ let reg = /[@%&$#;!]/gi;
  console.log(sentence1.replace(reg, ''))
 
  //4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
@@ -169,6 +169,8 @@ let becauseSentence = 'You cannot end a sentence with because because because is
 
  total = Number(salary[0]) + Number(salary[1]) + Number(salary[2]);
  console.log(total);
+
+
 
 // let sum  = 0;
 // for(i=0;i<salary.length;i++){
