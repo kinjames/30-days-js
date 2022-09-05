56e9489bcb742ccce5f80784738924bb9eee7d32
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-09-05 21:55:53 +0000

    update

--- day06/script.js
+++ day06/script.js
@@ -120,11 +120,6 @@ for (let i = 0; i <= 100; i++){
 }
 
 //10.Use for loop to iterate from 0 to 100 and print the sum of all numbers.
-for (let i = 0; i <= 100; i++){
-    let nums = i;
-    let arr = [];
-    arr=arr.push(nums)
-    console.log(arr)
-}
+
 
 //11.
