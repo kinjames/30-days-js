4525798bb4456fa0460244f04efd1522d6c9b3f4
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-09-08 20:16:44 +0000

    update

--- day06/script.js
+++ day06/script.js
@@ -121,19 +121,28 @@ for (let i = 0; i <= 100; i++){
 
 //10.Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 let arr = [];
-for(i=0;i<=100;i++){
+let sum = 0
+ for( let i=0;i<=100;i++){
     arr.push(i);
-    let sum = 0;
-    // for (const num of arr) {
-    //     sum = sum + num  
-
-    // }
-    for(let j = 0; j < arr.length; j++){
-        sum += arr[j]
-    }
-
-    console.log(sum)
 }
+
+for(let i = 0; i < arr.length; i++){
+    sum +=arr[i];
+}
+
+console.log(sum);
+
 
 
 //11.
+let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+let even = 0;
+let odd = 0;
+for(let i = 0; i < arr_1.length; i++) {
+if(i % 2 === 0) {
+even += arr_1[i];
+} else {
+odd += arr_1[i];
+}
+} 
+console.log([even, odd]);   
