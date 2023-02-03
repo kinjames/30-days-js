4d787d00503293cbd4d8cce6563e8c2ffa029a61
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-02-03 21:23:39 +0000

    update

--- day12/script.js
+++ day12/script.js
@@ -1,3 +1,10 @@
 // REGULAR EXPRESSIONS
 // Exercise Level 1
 // 1. Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
+
+// 2.The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+/* 
+points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
+distance = 12
+*/
