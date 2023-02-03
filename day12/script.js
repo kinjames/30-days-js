529c8e55eb16825241dab1bc48670b0f40d3776e
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-02-03 21:36:27 +0000

    update

--- day12/script.js
+++ day12/script.js
@@ -9,10 +9,11 @@ sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
 distance = 12
 */
 
-// 3.Write a pattern which identify if a string is a valid JavaScript variable
-/* 
-is_valid_variable('first_name') # True
-is_valid_variable('first-name') # False
-is_valid_variable('1first_name') # False
-is_valid_variable('firstname') # True
-*/
+// 3.Write a pattern which identify if a string is a valid JavaScript variable 
+// is_valid_variable('first_name') # True
+/^[a-zA-Z_$][a-zA-Z_$0-9]*$/
+
+// is_valid_variable('first-name') # False
+// is_valid_variable('1first_name') # False
+// is_valid_variable('firstname') # True
+
