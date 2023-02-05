e9a04f1c7397e979df19fbd27991df8fb83edaa7
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-02-05 22:31:56 +0000

    update

--- day13/script.js
+++ day13/script.js
@@ -2,3 +2,9 @@ console.log('James Kofi Myers');
 
 // Console Object Methods
 // Exercise Level One
+
+// 1.Display the countries array as a table
+
+// 2. Display the countries object as a table
+
+// 3. Use console.group() to group logs
