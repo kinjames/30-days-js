b78818590ee19bbe759661dc2287555a075cb2f0
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-03-23 21:55:11 +0000

    Add files via upload

--- day05/script.js
+++ day05/script.js
@@ -1,189 +1,162 @@
-//Arrays
 
-//Exercise Level 1
+// Exercise Level 1
 
-//1.Declare an empty array;
-let arr = new Array();
-console.log(arr);
+// 1. Declare an empty array;
+let myArr = [];
 
-let arr_1 = [];
-console.log(arr_1);
+// 2. Declare an array with more than 5 number of elements.
+let shoppingCart = ["Milk","Tomatoes","Chicken","Nuts","Beef","Apple","Pie"];
 
-//2.Declare an array with more than 5 number of elements
-let myDetails = ["James", 24, false, "Myers", "Kofi", 1998, 7, 5];
+// 3. Find the length of your array.
+console.log(shoppingCart.length);
 
-//3.Find the length of your array
-console.log(myDetails.length);
+// 4. Get the first item, the middle item and the last item of the array
+// first item
+let firstItem = shoppingCart[0];
+console.log (firstItem);
+// middle item
+let middleIndex = Math.floor(shoppingCart.length/2);
+let middleItem = shoppingCart[middleIndex];
+console.log(middleItem);
+// Last Item
+let lastIndex = shoppingCart.length - 1;
+let lastItem = shoppingCart[lastIndex];
+console.log(lastItem);
 
-//4.Get the first item, the middle item and the last item of the array
-let firstItem = myDetails[0];
-let middleItem = myDetails[4];
-let lastItem = myDetails[myDetails.length-1];
-
-console.log(firstItem,middleItem,lastItem);
-
-//5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
-let mixedDataTypes = [1, true, null, "apple", "banana", false];
+// 5. Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [true,"hello",5,null,"world",25];
 console.log(mixedDataTypes.length);
 
-//6.Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
-let itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
 
-//7.Print the array using console.log()
+// 7. Print the array using console.log()
 console.log(itCompanies);
 
-//8.Print the number of companies in the array
+// 8.  Print the number of companies in the array
 console.log(itCompanies.length);
 
-//9.Print the first company, middle and last company
+// 9. Print the first company, middle and last company
+// First Company
 let firstCompany = itCompanies[0];
-let middleCompany = itCompanies[3];
-let lastCompany = itCompanies[itCompanies.length - 1];
+console.log(firstCompany);
+// Middle company
+let middleCompanyIndex = Math.floor(itCompanies.length/2);
+let middleCompany = itCompanies[middleCompanyIndex];
+console.log(middleCompany);
+// Last Company
+let lastCompanyIndex = itCompanies.length - 1;
+let lastCompany = itCompanies[lastCompanyIndex];
+console.log(lastCompany);
 
-console.log(firstCompany,middleCompany,lastCompany);
-
-//10.Print out each company
-for(i=0;i<itCompanies.length;i++){
-    console.log(itCompanies[i]);
-}
-
-//Another Method
-for(count of itCompanies){
-    console.log(count);
-}
-
-//11.Change each company name to uppercase one by one and print them out
-for(i=0;i<itCompanies.length;i++){
-    console.log(itCompanies[i].toUpperCase())
-};
-// Another Method
-for (counts of itCompanies){
-    console.log(counts.toUpperCase());
-}
-
-//12.Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
-console.log(itCompanies.toString());
-
-//13.Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found.
-let google = itCompanies[1];
-
-let available_1 = google === -1 ? `Company not found` : `${google}`;
-console.log(available_1);// second method
-let available = itCompanies.includes(google) ? `${google}` : "Company not found";
-console.log(available);
-
-//14.Filter out companies which have more than one 'o' without the filter method;
-for(i=0;i<=itCompanies.length-1;i++){
-    console.log(itCompanies[i].includes('oo'))
+//  10. Print out each company
+for (let i=0; i < itCompanies.length; i++){
+    let eachCompany = itCompanies[i];
+    console.log(eachCompany);
 };
 
-//15.Sort the array using sort() method
-console.log(itCompanies.sort());
-
-//16.Reverse the array using reverse() method
-console.log(itCompanies.reverse());
-
-//17.Slice out the first 3 companies from the array
-console.log(itCompanies.slice(0,3));
-
-//18.Slice out the last 3 companies from the array
-console.log(itCompanies.slice(4,7));
-
-//19.Slice out the middle IT company or companies from the array.
-console.log(itCompanies.splice(3,1))
-
-//20.Remove the first IT company from the array
-console.log(itCompanies.shift());
-
-//21.Remove the middle IT company or companies from the array
-console.log(itCompanies.splice(2,1));
-
-//22.Remove the last IT company from the array
-console.log(itCompanies.pop());
-
-//23.Remove all IT companies
- itCompanies =[];
- console.log(itCompanies);
+// alternate
+let eachCompanyAlt = itCompanies.forEach((item)=>{
+    console.log(item);
+});
 
 
+//  11. Change each company name to uppercase one by one and print them out
+for (let i = 0; i < itCompanies.length; i++){
+    let companyToUpperCase = itCompanies[i].toUpperCase();
+    console.log(companyToUpperCase);
+};
 
- //Exercise Level 2
- //1.Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file.
+// Alternate
+let companyToUpperCaseAlt = itCompanies.forEach((item)=>{
+    let result = item.toUpperCase();
+    console.log(result);
+});
 
+//  12. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+let arrayToSentence = itCompanies.toString();
+console.log(arrayToSentence);
 
- //2.First remove all the punctuations and change the string to array and count the number of words in the array
-let sentence= 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-sentence = sentence.replace(/[,.]/g ,'');
-let result = sentence.split(" ");
-console.log(result.length);
+// Alternate
+let arrayToSentenceAlt = itCompanies.join();
+console.log(arrayToSentenceAlt);
 
-//3.In the following shopping cart add, remove, edit items
-const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+//  13. Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+let companyExist = itCompanies.includes('Tesla');  //false
+let companyExistTrue = itCompanies.includes('Apple'); // true
+console.log(companyExist,companyExistTrue);
 
-//i.add 'Meat' in the beginning of your shopping cart if it has not been already added
-shoppingCart.unshift('Meat');
-console.log(shoppingCart);
+// 14. Filter out companies which have more than one 'o' without the filter method
+let filteredArray = [];
+for(let i=0;i<itCompanies.length;i++){
+    if(itCompanies[i].includes('oo')){
+        filteredArray.push(itCompanies[i]);
+    };
+};
+console.log(filteredArray);
 
-//ii.add Sugar at the end of you shopping cart if it has not been already added
-shoppingCart.push('Sugar');
-console.log(shoppingCart);
-
-//iii.remove 'Honey' if you are allergic to honey
-console.log(shoppingCart.splice(shoppingCart.indexOf('Honey'),1));
-
-
-//iv.modify Tea to 'Green Tea' 
-let tea = shoppingCart.indexOf("Tea");
-shoppingCart[tea] = "Green Tea"
-console.log(shoppingCart)
-
-
-//4.In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
-// if (countries.includes("Ethiopia")){
-//     console.log("ETHIOPIA")
-// }else {
-//     countries.push("Ethiopia")
-// };
-
-//5.In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
-
-// if (webTechs.includes("Sass")){
-//     console.log('Sass is a CSS preprocess')
-// }else {
-//     webTechs.push('Sass');
-//     console.log(webTechs);
-// }
-
-//6.Concatenate the following two variables and store it in a fullStack variable.
-const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-const backEnd = ['Node','Express', 'MongoDB']
-
-let fullStack = frontEnd.concat(backEnd);
-console.log(fullStack);
+// Alternate
+let filteredArrayAlt = [];
+itCompanies.forEach((item)=>{
+    if(item.includes('oo')){
+        filteredArrayAlt.push(item);
+    }
+});
+console.log(filteredArrayAlt);
 
 
-//Exercise Level 3
+// 15. Sort the array using sort() method
+let sortedCompany = itCompanies.sort();
+console.log(sortedCompany);
 
-//1.The following is an array of 10 students ages:
-const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+// 16. Reverse the array using reverse() method
+let reverseCompany = itCompanies.reverse();
+console.log(reverseCompany);
 
-//i.Sort the array and find the min and max age
-let age = ages.sort();
-console.log(age);
-let maxNum = age[9]
-let minNum = age[0]
+// 17. Slice out the first 3 companies from the array
+let firstThreeSlice = itCompanies.slice(0,3);
+console.log(firstThreeSlice);
 
-console.log("Min Number is:" + age[0], "Max num is:" + age[9]);
-//In order to find the min and max of numbers, we can use Math.min(num) and Math.max(num)
+// 18. Slice out the last 3 companies from the array
+let lastThreeSlice = itCompanies.slice(-3);
+console.log(lastThreeSlice);
 
-//ii.Find the median age(one middle item or two middle items divided by two)
-let midNum_1 = age[4]
-let midNum_2 = age[5]
-let medianNum = (midNum_1 + midNum_2)/ 2;
-console.log(medianNum)
+// 19. Slice out the middle IT company or companies from the array
+let sliceMiddleIndex = Math.floor(itCompanies.length / 2);
+let sliceMiddle = itCompanies.slice(sliceMiddleIndex,sliceMiddleIndex + 1); 
+console.log(sliceMiddle);
 
-//iii.Find the range of the ages(max minus min)
-let range = maxNum - minNum;
-console.log(range)
+// 20. Remove the first IT company from the array
+let removeFirstCompany = itCompanies.shift();
+console.log(removeFirstCompany);
+console.log(itCompanies);
 
-//iv.Compare the value of (min - average) and (max - average), use abs() method;
+// 21. Remove the middle IT company or companies from the array
+let sliceMiddleIndexa; //work on it and fix it
+let removeMiddleCompanies;
+    if(itCompanies.length % 2 === 0){
+        sliceMiddleIndexa = (itCompanies.length / 2) - 1;
+        removeMiddleCompanies = itCompanies.splice(sliceMiddleIndexa,2);
+        console.log(removeMiddleCompanies);
+    } else {
+        sliceMiddleIndexa = itCompanies.length / 2;
+        removeMiddleCompanies = itCompanies.splice(sliceMiddleIndexa,1);
+        console.log(removeMiddleCompanies);
+    };
+
+// console.log(sliceMiddlea);
+
+// 22. Remove the last IT company from the array
+let removeLastCompany = itCompanies.pop();
+console.log(removeLastCompany);
+
+// 23. Remove all IT companies
+    itCompanies.length = 0;
+    console.log(itCompanies);
+
+// Alternate
+    itCompanies.splice(0,itCompanies.length);
+    console.log(itCompanies);
+
+
+
