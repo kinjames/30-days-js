be73121b1ad58e11a4efcb4d6c0d9d71442f0932
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-03-23 21:54:08 +0000

    Add files via upload

---
+++ day05/main.js
@@ -0,0 +1,17 @@
+import {
+    countries
+} from './countries.js';
+
+import {
+    webTechs
+} from './web_techs.js';
+
+
+
+// Exercise Level 2
+
+// 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+console.log(countries,webTechs);
+
+// 2. First remove all the punctuations and change the string to array and count the number of words in the array
+
