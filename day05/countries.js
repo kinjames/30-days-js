61d1b7a3b591a3273f5a99fc524b6e9fd1c7bcca
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-03-23 21:53:13 +0000

    Add files via upload

--- day05/countries.js
+++ day05/countries.js
@@ -1,4 +1,4 @@
-const countries = [
+export const countries = [
     'Albania',
     'Bolivia',
     'Canada',
@@ -10,30 +10,4 @@ const countries = [
     'Ireland',
     'Japan',
     'Kenya'
-  ];
-
-//1.1.Slice the first ten countries from the countries array
-console.log(countries.slice(0,10));
-
-//2.Find the middle country(ies) in the countries array
-console.log(countries.length);
-console.log(countries[5]);
-
-//3.Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
-let set_1 = countries.slice(0,countries.length/2);
-let set_2 = countries.slice(countries.length/2, countries.length-1);
-console.log(set_1,set_2)
-
-if (countries.length%2 ===0){
-  // let set_1 = countries.slice(0,countries.length/2);
-  // let set_2 = countries.slice(countries.length/2, countries.length-1);
-  console.log(set_1,set_2)
-} else {
-  countries.push("Ghana");
-  console.log(countries)
-}
-
-//Listing all the members of an array without using the index
-for( const count of countries){
-  console.log(count);
-}
+    ];
