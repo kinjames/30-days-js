f4a2cb6f0ffc3a403d63304c73be9bc0f42510ce
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-03-24 20:14:11 +0000

    Add files via upload

--- day05/main.js
+++ day05/main.js
@@ -14,4 +14,50 @@ import {
 console.log(countries,webTechs);
 
 // 2. First remove all the punctuations and change the string to array and count the number of words in the array
+/* 
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(words)
+console.log(words.length)
+*/
+let text = "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+let removePunctuations = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+text = text.replace(removePunctuations);
+let myArray = text.split(' ');
+console.log(myArray, myArray.length);
+
+// 3. In the following shopping cart add, remove, edit items
+let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+
+// add 'Meat' in the beginning of your shopping cart if it has not been already added.
+if (shoppingCart.includes('Meat')){
+    console.log(shoppingCart);
+} else {
+    shoppingCart.unshift('Meat');
+    console.log(shoppingCart);
+};
+
+// add Sugar at the end of you shopping cart if it has not been already added
+if (shoppingCart.includes('Sugar')){
+    console.log(shoppingCart);
+} else {
+    shoppingCart.push('Sugar');
+    console.log(shoppingCart);
+};
+
+// remove 'Honey' if you are allergic to honey
+let allergicToHoney = false;
+
+if(allergicToHoney){
+    let indexOfHoney  = shoppingCart.indexOf('Honey');
+    shoppingCart.splice(indexOfHoney,1);
+};
+console.log(shoppingCart);
+
+// modify Tea to 'Green Tea'
+shoppingCart[shoppingCart.indexOf('Tea')] = 'Green Tea';
+console.log(shoppingCart);
+
+
+
 
