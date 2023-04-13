f4e8ee8058d2c0f0291c4f67bf54ad2dd4b03949
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-13 22:10:22 +0000

    Add files via upload

---
+++ day01/datatypes.js
@@ -0,0 +1,15 @@
+// 5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let stringData = 'James';
+let booleanData = true;
+let undefinedData ;
+let nullData = null;
+let num = 3;
+let symb = Symbol(8);
+
+console.log(typeof stringData);
+console.log(typeof booleanData);
+console.log(typeof nullgData);
+console.log(typeof undefinedgData);
+console.log(typeof num);
+console.log(typeof symb);
