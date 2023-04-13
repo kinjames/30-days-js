b3cb7f878243ba220405076f0d0833ecd243d91d
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-13 22:09:53 +0000

    Add files via upload

---
+++ day01/variable.js
@@ -0,0 +1,6 @@
+// 4.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let stringData = 'James';
+let booleanData = true;
+let undefinedData ;
+let nullData = null;
