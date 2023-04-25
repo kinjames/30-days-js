2e7676521a0aa9823667b4a39202ee6f71e35d35
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-25 11:30:11 +0000

    update all

--- day02/script.js
+++ day02/script.js
@@ -3,7 +3,7 @@
 //Exercise Level 1
 
 //1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
-let challenge = '30 Days Of JavaScript';
+let challenge = "30 Days Of JavaScript";
 
 //2.Print the string on the browser console using console.log()
 console.log(challenge);
@@ -18,80 +18,80 @@ console.log(challenge.toUpperCase());
 console.log(challenge.toLocaleLowerCase());
 
 //6.Cut (slice) out the first word of the string using substr() or substring() method
-console.log(challenge.substring(0,3));
+console.log(challenge.substring(0, 3));
 
 //7.Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
 //To get this, we can find the length of the sentence and also the index of the starting word
 console.log(challenge.length); //21
-console.log(challenge.indexOf('Day')) //3
-console.log(challenge.substring(3,21)); 
+console.log(challenge.indexOf("Day")); //3
+console.log(challenge.substring(3, 21));
 
 //8.Check if the string contains a word Script using includes() method
-console.log(challenge.includes('Script')); 
+console.log(challenge.includes("Script"));
 
 //9.Split the string into an array using split() method.
 console.log(challenge.split());
 
 //10.Split the string 30 Days Of JavaScript at the space using split() method
-console.log(challenge.split(' '));
+console.log(challenge.split(" "));
 
 //11.'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
-let techGurus = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
-console.log(techGurus.split(','));
+let techGurus = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(techGurus.split(","));
 
 //12.Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
 console.log(challenge.replace("JavaScript", "Python"));
 
 //13.What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
 console.log(challenge.charAt(15));
-console.log(challenge[15]);//Another method to use in place of charAt();
+console.log(challenge[15]); //Another method to use in place of charAt();
 
 //14.What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
-console.log(challenge.charCodeAt(challenge.indexOf('J')));
+console.log(challenge.charCodeAt(challenge.indexOf("J")));
 
 //15.Use indexOf to determine the position of the first occurrence of 'a' in 30 Days Of JavaScript
-console.log(challenge.indexOf('a'));
+console.log(challenge.indexOf("a"));
 
 //16.Use lastIndexOf to determine the position of the last occurrence of 'a' in 30 Days Of JavaScript.
-console.log(challenge.lastIndexOf('a'));
+console.log(challenge.lastIndexOf("a"));
 
 //17.Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-let sentence = 'You cannot end a sentence with because because because is a conjunction';
-console.log(sentence.indexOf('because'));
+let sentence =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(sentence.indexOf("because"));
 
 //18.Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-console.log(sentence.lastIndexOf('because'));
+console.log(sentence.lastIndexOf("because"));
 
 //19.Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-console.log(sentence.search('because'));
+console.log(sentence.search("because"));
 
 //20.Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
 console.log(challenge.trim());
 
 //21.Use startsWith() method with the string 30 Days Of JavaScript and make the result true.
-console.log(challenge.startsWith('30 Days Of JavaScript'));
+console.log(challenge.startsWith("30 Days Of JavaScript"));
 
 //22.Use endsWith() method with the string 30 Days Of JavaScript and make the result true.
-console.log(challenge.endsWith('30 Days Of JavaScript'));
+console.log(challenge.endsWith("30 Days Of JavaScript"));
 
 //23.Use match() method to find all the a’s in 30 Days Of JavaScript
-let pattern = /a/gi
+let pattern = /a/gi;
 console.log(challenge.match(pattern));
 
 //24.Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
-let statement1 = '30 Days Of ';
-let statement2 = 'JavaScript';
+let statement1 = "30 Days Of ";
+let statement2 = "JavaScript";
 let fullStatement = statement1 + statement2;
 
 //25.Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log(challenge.repeat(2));
 
-
-
 //Exercise Level 2
 
 //1.Using console.log() print out the following statement:
-let quote = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+let quote =
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
 console.log(quote);
 
 //2.Using console.log() print out the following quote by Mother Teresa:
@@ -99,22 +99,22 @@ let quote_2 = `"Love is not patronizing and charity isn't about pity, it is abou
 console.log(quote_2);
 
 //3.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
-console.log(typeof('10') == typeof(10)); //False
-console.log(typeof(Number('10')) == typeof(10));  //True
+console.log(typeof "10" == typeof 10); //False
+console.log(typeof Number("10") == typeof 10); //True
 
 //4.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
 console.log(parseFloat(9.8) == 10); //False
 console.log(parseFloat(Math.round(9.8)) == 10); //True  // You can also use ceil to round up.
 
 //5.Check if 'on' is found in both python and jargon
-console.log("python".includes('on') && "jargon".includes('on')); // True
+console.log("python".includes("on") && "jargon".includes("on")); // True
 
 //6.I hope this course is not full of jargon. Check if jargon is in the sentence.
-let someSentence = 'I hope this course is not full of jargon.'
-console.log(someSentence.includes('jargon')); //True
+let someSentence = "I hope this course is not full of jargon.";
+console.log(someSentence.includes("jargon")); //True
 
 //7. Generate a random number between 0 and 100 inclusively.
-let num = Math.floor(Math.random()*101);
+let num = Math.floor(Math.random() * 101);
 console.log(num);
 
 //8.Generate a random number between 50 and 100 inclusively.
@@ -124,7 +124,7 @@ let ranBtn = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 console.log(ranBtn);
 
 //9.Generate a random number between 0 and 255 inclusively.
-let num2 = Math.floor(Math.random()*255);
+let num2 = Math.floor(Math.random() * 255);
 console.log(num2);
 
 //10.Access the 'JavaScript' string characters using a random number.
@@ -134,55 +134,43 @@ let courseChar = course[ranNum];
 console.log(courseChar);
 
 //11.Use console.log() and escape characters to print the following pattern.
-let numbers = "1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125";
+let numbers =
+  "1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125";
 console.log(numbers);
 
 //12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
-let someStatement = 'You cannot end a sentence with because because because is a conjunction';
-console.log(someStatement.slice(someStatement.indexOf('because'),55));
-
+let someStatement =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(someStatement.slice(someStatement.indexOf("because"), 55));
 
 //Exercise Level 3
 
 //1.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
-let loveSentence = "'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence."
+let loveSentence =
+  "'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.";
 
-let pattern_2 = /love/gi
+let pattern_2 = /love/gi;
 console.log(loveSentence.match(pattern_2));
 
 //2.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-let becauseSentence = 'You cannot end a sentence with because because because is a conjunction';
- let regEx = /because/gi;
- 
- console.log(becauseSentence.match(regEx));
+let becauseSentence =
+  "You cannot end a sentence with because because because is a conjunction";
+let regEx = /because/gi;
 
- //3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
- const sentence1 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(becauseSentence.match(regEx));
 
- let reg = /[@%&$#;!]/gi;
- console.log(sentence1.replace(reg, ''));
+//3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+const sentence1 =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
 
- //4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
- let incomeText = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
- let incomeRegEx = /\d+/g
- let salary = incomeText.match(incomeRegEx);
+let reg = /[@%&$#;!]/gi;
+console.log(sentence1.replace(reg, ""));
 
- total = Number(salary[0]) + Number(salary[1]) + Number(salary[2]);
- console.log(total);
+//4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let incomeText =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+let incomeRegEx = /\d+/g;
+let salary = incomeText.match(incomeRegEx);
 
-
-
-// let sum  = 0;
-// for(i=0;i<salary.length;i++){
-//     salary[i]= Number(salary[i]);
-//     sum = sum + salary[i];
-//     console.log(sum);
-// } This iterate the numbers.
-//This is the second thing
-
-
-
-
-
-
-
+total = Number(salary[0]) + Number(salary[1]) + Number(salary[2]);
+console.log(total);
--- day04/script.js
+++ day04/script.js
@@ -4,9 +4,12 @@
 
 //1.Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
-let userInput = Number(prompt('Enter your age:'));
+let userInput = Number(prompt("Enter your age:"));
 
-result = userInput >= 18 ? "You are old enough to drive" : `You are left with ${18 - userInput} years to drive.`;
+result =
+  userInput >= 18
+    ? "You are old enough to drive"
+    : `You are left with ${18 - userInput} years to drive.`;
 console.log(result);
 
 //2.Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
@@ -14,23 +17,24 @@ console.log(result);
 let myAge = 24;
 let userAge = Number(prompt("How old are you?"));
 
-if (myAge > userAge){
-    console.log(`I am ${myAge - userAge} years older than you.`);
-} else if (myAge === userAge){
-    console.log("We are of the same age.")
+if (myAge > userAge) {
+  console.log(`I am ${myAge - userAge} years older than you.`);
+} else if (myAge === userAge) {
+  console.log("We are of the same age.");
 } else {
-    console.log(`You are ${userAge - myAge} years older than me.`)
+  console.log(`You are ${userAge - myAge} years older than me.`);
 }
 
 //3.If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 //i.using if else.
-let a = 4, b = 6;
+let a = 4,
+  b = 6;
 
-if ( a > b){
-    console.log("a is greater than b")
+if (a > b) {
+  console.log("a is greater than b");
 } else {
-    console.log("a is less than b")
-};
+  console.log("a is less than b");
+}
 
 //ii. ternary operator.
 let num = a > b ? "a is greater than b" : "a is less than b";
@@ -39,67 +43,66 @@ console.log(num);
 //4.Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
 let ranNum = Math.floor(Math.random() * 11);
 
-if (ranNum % 2 == 0){
-    console.log(`${ranNum} is an even number`);
+if (ranNum % 2 == 0) {
+  console.log(`${ranNum} is an even number`);
 } else {
-    console.log(`${ranNum} is an odd number`);
+  console.log(`${ranNum} is an odd number`);
 }
-
 
 //Exercise Level 2
 
 //1.Write a code which can give grades to students according to theirs scores:
- let grade = Number(prompt('What is your score:'));
-if (grade >= 80 && grade <= 100){
-    console.log(`${grade}: grade A`)
-} else if (grade >=70 && grade <= 89){
-    console.log(`${grade}: grade B`)
-} else if (grade >=60 && grade <= 79){
-    console.log(`${grade}: grade C`)
-}else if (grade >= 50 && grade <= 59){
-    console.log(`${grade}: grade D`)
-} else if (grade >= 0 && grade <= 49){
-    console.log(`${grade}: grade F`)
+let grade = Number(prompt("What is your score:"));
+if (grade >= 80 && grade <= 100) {
+  console.log(`${grade}: grade A`);
+} else if (grade >= 70 && grade <= 89) {
+  console.log(`${grade}: grade B`);
+} else if (grade >= 60 && grade <= 79) {
+  console.log(`${grade}: grade C`);
+} else if (grade >= 50 && grade <= 59) {
+  console.log(`${grade}: grade D`);
+} else if (grade >= 0 && grade <= 49) {
+  console.log(`${grade}: grade F`);
 } else {
-    console.log("Wrong Score");
+  console.log("Wrong Score");
 }
 
-
 //2.Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
-let weather = prompt('What month is it?');
+let weather = prompt("What month is it?");
 weather = weather.toLocaleLowerCase();
 
 switch (weather) {
-    case 'september':
-    case 'october':
-    case 'novermber': console.log("Autumn");        
-        break;
-    case 'december':
-    case 'january':
-    case 'february': console.log('Winter');            
+  case "september":
+  case "october":
+  case "novermber":
+    console.log("Autumn");
     break;
-    case 'march':
-    case 'april':
-    case 'may': console.log('Spring');
+  case "december":
+  case "january":
+  case "february":
+    console.log("Winter");
     break;
-    default: console.log('Summer')
-
+  case "march":
+  case "april":
+  case "may":
+    console.log("Spring");
+    break;
+  default:
+    console.log("Summer");
 }
 
-
 //3.Check if a day is weekend day or a working day. Your script will take day as an input.
-let dayOfWeek = prompt('What is the day  today?');
+let dayOfWeek = prompt("What is the day  today?");
 dayOfWeek = dayOfWeek.toLowerCase();
 
 switch (dayOfWeek) {
-    case 'saturday':
-    case 'sunday':
-        console.log(`${dayOfWeek} is a weekend`)    
+  case "saturday":
+  case "sunday":
+    console.log(`${dayOfWeek} is a weekend`);
     break;
-    default:
-    console.log(`${dayOfWeek} is a working day`)
+  default:
+    console.log(`${dayOfWeek} is a working day`);
 }
-
 
 //Exercise Level 3
 //1.Write a program which tells the number of days in a month.
@@ -107,14 +110,15 @@ switch (dayOfWeek) {
 let monthOfYear = prompt("Enter a month:").toLowerCase();
 
 switch (monthOfYear) {
-    case 'september':
-    case 'april':
-    case 'june':
-    case 'november':
-        console.log(`${monthOfYear} has 30 days.`);
+  case "september":
+  case "april":
+  case "june":
+  case "november":
+    console.log(`${monthOfYear} has 30 days.`);
     break;
-    case 'february': console.log(`${monthOfYear} has 28 days.`)
+  case "february":
+    console.log(`${monthOfYear} has 28 days.`);
     break;
-    default:
-        console.log(`${monthOfYear} has 31 days.`)
+  default:
+    console.log(`${monthOfYear} has 31 days.`);
 }
