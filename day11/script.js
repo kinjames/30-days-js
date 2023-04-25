4949898463baa42863b777bc1078f2926cb4422d
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-25 11:20:29 +0000

    update

--- day11/script.js
+++ day11/script.js
@@ -103,16 +103,27 @@ for (const country of countriess) {
 }
 
 // 2.A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
-const studentOne = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
-let [name, skills, scores] = studentOne;
-let [htmlScore, cssScore, jsScore, reactScore] = scores;
-console.log(name, skills, jsScore, reactScore);
+// const studentOne = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
+// let [name, skills, scores] = studentOne;
+// let [htmlScore, cssScore, jsScore, reactScore] = scores;
+// console.log(name, skills, jsScore, reactScore);
 
 // 3.Write a function called convertArrayToObject which can convert the array to a structure object.
 const students = [
   ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]], //name,skills,scores
   ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
 ];
+
+const convertArrayToObject = (students) => {
+  let result = [];
+  for (let student of students) {
+    const [name, skills, scores] = student;
+    const eachStud = { name, skills, scores };
+    result.push(eachStud);
+  }
+  return result;
+};
+console.log(convertArrayToObject(students));
 
 // 4.Copy the student object to newStudent without mutating the original object. In the new object add the following ?
 const student = {
@@ -136,7 +147,13 @@ const student = {
 
 const newstudent = { ...student };
 console.log(newstudent);
+let { name, age, skills } = student;
 // Add Bootstrap with level 8 to the front end skill sets
+skills.frontEnd.push({ skill: "Bootstrap", level: 8 });
 // Add Express with level 9 to the back end skill sets
+skills.backEnd.push({ skill: "Express", level: 9 });
 // Add SQL with level 8 to the data base skill sets
+skills.dataBase.push({ skill: "SQL", level: 8 });
 // Add SQL without level to the data science skill sets
+skills.dataScience.push("SQL");
+console.log(newstudent);
