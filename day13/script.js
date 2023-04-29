5c2f492807dc03755c19baa6c137d32d1052d974
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-29 18:49:25 +0000

    update

--- day13/script.js
+++ day13/script.js
@@ -1,4 +1,4 @@
-console.log('James Kofi Myers');
+console.log("James Kofi Myers");
 
 // Console Object Methods
 // Exercise Level One
@@ -8,3 +8,13 @@ console.log('James Kofi Myers');
 // 2. Display the countries object as a table
 
 // 3. Use console.group() to group logs
+
+// Exercises:Level 2
+// 1. 10 > 2 * 10 use console.assert()
+
+// 2. Write a warning message using console.warn()
+
+// 3. Write an error message using console.error()
+
+// Exercises:Level 3
+// 1. Check the speed difference among the following loops: while, for, for of, forEach
