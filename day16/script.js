3014e62d3e6655d8cb6cbd4c3ab79cdfdc1bf69a
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-05-12 20:47:56 +0000

    update

--- day16/script.js
+++ day16/script.js
@@ -107,3 +107,9 @@ const txt = `{
 }
 `;
 // 1.Change skills array to JSON using JSON.stringify()
+
+// 2.Stringify the age variable
+
+// 3.Stringify the isMarried variable
+
+// 4.Stringify the student object
