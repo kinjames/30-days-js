79fd652c2e230017885c7be682cc123bc2e53466
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-05-26 20:23:34 +0000

    update

--- day18/script.js
+++ day18/script.js
@@ -6,4 +6,8 @@
 // 1.Print out all the cat names in to catNames variable.
 
 // Exercise Level 3
-// 1.
+// 1.Read the cats api and find the average weight of cat in metric unit.
+
+// 2.Read the countries api and find out the 10 largest countries
+
+// 3.
