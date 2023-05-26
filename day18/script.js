ce8307ab8eef71705a41964e452c1137f440a601
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-05-26 20:22:28 +0000

    update

--- day18/script.js
+++ day18/script.js
@@ -2,4 +2,5 @@
 // Exercise Level 1
 // 1.Read the countries API using fetch and print the name of country, capital, languages, population and area.
 
+// Exercise Level 2
 // 1.Print out all the cat names in to catNames variable.
