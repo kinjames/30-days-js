5905b2544e37694bb0277e7192c0f68ac118ed4b
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-05-30 16:55:46 +0000

    update

--- day18/script.js
+++ day18/script.js
@@ -11,3 +11,22 @@
 // 2.Read the countries api and find out the 10 largest countries
 
 // 3.Read the countries api and count total number of languages in the world used as officials.
+
+// Color Palette 1: Elegant and Modern
+
+// Primary Color: #354F52 (Dark Teal)
+// Secondary Color: #D3D0CB (Light Gray)
+// Accent Color: #FF5851 (Coral Red)
+// Background Color: #FFFFFF (White)
+// Color Palette 2: Vibrant and Energetic
+
+// Primary Color: #FF5733 (Orange)
+// Secondary Color: #54C571 (Green)
+// Accent Color: #3A4BC3 (Royal Blue)
+// Background Color: #F7F7F7 (Light Gray)
+// Color Palette 3: Minimalist and Clean
+
+// Primary Color: #2E2E2E (Dark Gray)
+// Secondary Color: #FFFFFF (White)
+// Accent Color: #FFB830 (Golden Yellow)
+// Background Color: #F0F0F0 (Light Gray)
