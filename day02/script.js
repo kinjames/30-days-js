2fd0d39fcd5398803c786e33e7dd330cb4515978
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-06-09 22:01:25 +0000

    update

--- day02/script.js
+++ day02/script.js
@@ -15,7 +15,7 @@ console.log(challenge.length);
 console.log(challenge.toUpperCase());
 
 //5.Change all the string characters to lowercase letters using toLowerCase() method.
-console.log(challenge.toLocaleLowerCase());
+console.log(challenge.toLowerCase());
 
 //6.Cut (slice) out the first word of the string using substr() or substring() method
 console.log(challenge.substring(0, 3));
@@ -70,10 +70,10 @@ console.log(sentence.search("because"));
 console.log(challenge.trim());
 
 //21.Use startsWith() method with the string 30 Days Of JavaScript and make the result true.
-console.log(challenge.startsWith("30 Days Of JavaScript"));
+console.log(challenge.startsWith("30"));
 
 //22.Use endsWith() method with the string 30 Days Of JavaScript and make the result true.
-console.log(challenge.endsWith("30 Days Of JavaScript"));
+console.log(challenge.endsWith("JavaScript"));
 
 //23.Use match() method to find all the a’s in 30 Days Of JavaScript
 let pattern = /a/gi;
@@ -83,6 +83,7 @@ console.log(challenge.match(pattern));
 let statement1 = "30 Days Of ";
 let statement2 = "JavaScript";
 let fullStatement = statement1 + statement2;
+console.log(fullStatement);
 
 //25.Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log(challenge.repeat(2));
@@ -95,16 +96,19 @@ let quote =
 console.log(quote);
 
 //2.Using console.log() print out the following quote by Mother Teresa:
-let quote_2 = `"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`;
+let quote_2 =
+  "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"";
 console.log(quote_2);
 
 //3.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
-console.log(typeof "10" == typeof 10); //False
-console.log(typeof Number("10") == typeof 10); //True
+console.log(typeof "10" === typeof 10); //False
+console.log(typeof Number("10") === typeof 10); //True
+console.log(typeof +"10" === typeof 10); //True
+console.log(typeof parseInt("10") === typeof 10); //True
 
 //4.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-console.log(parseFloat(9.8) == 10); //False
-console.log(parseFloat(Math.round(9.8)) == 10); //True  // You can also use ceil to round up.
+console.log(parseFloat(9.8) === 10); //False
+console.log(parseFloat(Math.round(9.8)) === 10); //True  // You can also use ceil to round up.
 
 //5.Check if 'on' is found in both python and jargon
 console.log("python".includes("on") && "jargon".includes("on")); // True
@@ -124,11 +128,11 @@ let ranBtn = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 console.log(ranBtn);
 
 //9.Generate a random number between 0 and 255 inclusively.
-let num2 = Math.floor(Math.random() * 255);
+let num2 = Math.floor(Math.random() * 256);
 console.log(num2);
 
 //10.Access the 'JavaScript' string characters using a random number.
-let course = `JavaScript`;
+let course = "JavaScript";
 let ranNum = Math.floor(Math.random() * course.length - 1);
 let courseChar = course[ranNum];
 console.log(courseChar);
@@ -141,7 +145,7 @@ console.log(numbers);
 //12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
 let someStatement =
   "You cannot end a sentence with because because because is a conjunction";
-console.log(someStatement.slice(someStatement.indexOf("because"), 55));
+console.log(someStatement.substring(someStatement.indexOf("because"), 55));
 
 //Exercise Level 3
 
@@ -149,15 +153,16 @@ console.log(someStatement.slice(someStatement.indexOf("because"), 55));
 let loveSentence =
   "'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.";
 
-let pattern_2 = /love/gi;
-console.log(loveSentence.match(pattern_2));
+let pattern_2 = /love/g;
+let numOfLove = (loveSentence.match(pattern_2) || []).length;
+console.log(numOfLove);
 
 //2.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 let becauseSentence =
   "You cannot end a sentence with because because because is a conjunction";
-let regEx = /because/gi;
-
-console.log(becauseSentence.match(regEx));
+let regEx = /because/g;
+let numOfBecause = (becauseSentence.match(regEx) || []).length;
+console.log(numOfBecause);
 
 //3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
 const sentence1 =
@@ -166,11 +171,54 @@ const sentence1 =
 let reg = /[@%&$#;!]/gi;
 console.log(sentence1.replace(reg, ""));
 
-//4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
-let incomeText =
-  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
-let incomeRegEx = /\d+/g;
-let salary = incomeText.match(incomeRegEx);
+const sentence2 =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
 
-total = Number(salary[0]) + Number(salary[1]) + Number(salary[2]);
-console.log(total);
+// Clean the text by removing unwanted characters and symbols
+let cleanedText = sentence2.replace(/[^\w\s]/g, "");
+
+// Split the text into an array of words
+let words = cleanedText.split(/\s+/);
+
+// Create an object to store word frequency
+let wordFrequency = {};
+
+// Count the frequency of each word
+for (let word of words) {
+  if (wordFrequency[word]) {
+    wordFrequency[word]++;
+  } else {
+    wordFrequency[word] = 1;
+  }
+}
+
+// Find the most frequent word
+let mostFrequentWord = "";
+let maxFrequency = 0;
+for (let word in wordFrequency) {
+  if (wordFrequency[word] > maxFrequency) {
+    mostFrequentWord = word;
+    maxFrequency = wordFrequency[word];
+  }
+}
+
+console.log("Most frequent word:", mostFrequentWord);
+
+//4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const text =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+
+// Extract numbers using regular expression
+const numberss = text.match(/\d+/g);
+
+// Calculate the total annual income
+let totalIncome = 0;
+for (let number of numberss) {
+  totalIncome += parseInt(number);
+}
+
+// Multiply monthly earnings by 12 to get the annual amount
+totalIncome += parseInt(numberss[0]) * 12;
+totalIncome += parseInt(numberss[2]) * 12;
+
+console.log("Total annual income:", totalIncome, "euro");
