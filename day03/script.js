91540e85222f6a9ff978f29f998162c3857e8ec5
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-06-10 20:52:41 +0000

    update

--- day03/script.js
+++ day03/script.js
@@ -12,16 +12,24 @@ let age = 24;
 let isMarried = false;
 let year = 2022;
 
-console.log(typeof(firstName),typeof(lastName),typeof(country),typeof(city),typeof(age), typeof(isMarried),typeof(year),);
+console.log(
+  typeof firstName,
+  typeof lastName,
+  typeof country,
+  typeof city,
+  typeof age,
+  typeof isMarried,
+  typeof year
+);
 
 //2.Check if type of '10' is equal to 10
-let str = '10';
+let str = "10";
 let num = 10;
 
-console.log(str==num,str===num);//One will give true and the other will be false because the first only checks the value, but the second checks both value and data types.
+console.log(str == num, str === num); //One will give true and the other will be false because the first only checks the value, but the second checks both value and data types.
 
 //3.Check if parseInt('9.8') is equal to 10
-console.log(parseInt('9.8') == 10);
+console.log(parseInt("9.8") === 10);
 
 //4.Write three JavaScript statement which provide truthy value.
 //all positive integers
@@ -47,7 +55,15 @@ console.log(parseInt('9.8') == 10);
 4 === '4' //false
 */
 //Find the length of python and jargon and make a falsy comparison statement.
-console.log('python'.length != 'jargon'.length);
+let word1 = "python";
+let word2 = "jargon";
+
+let lengthWord1 = word1.length;
+let lengthWord2 = word2.length;
+
+let falsyComparison = lengthWord1 === lengthWord2;
+
+console.log(falsyComparison);
 
 //6.
 /*
@@ -86,40 +102,45 @@ console.log(dateNow.getMinutes());
 //vii.Find out the numbers of seconds elapsed from January 1, 1970 to now.
 console.log(dateNow.getTime());
 
-
 //Exercise Level 2
 //1.Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
-let widthNum = Number(prompt("What is the width")); 
+let widthNum = Number(prompt("What is the width"));
 let heightNum = Number(prompt("What is the height"));
 
 let totalArea = 0.5 * widthNum * heightNum;
 console.log(totalArea);
 
 //2.Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
-let aNum = Number(prompt('What is the value of a'));
-let bNum = Number(prompt('What is the value of b'));
-let cNum = Number(prompt('What is the value of c'));
+let aNum = Number(prompt("What is the value of a"));
+let bNum = Number(prompt("What is the value of b"));
+let cNum = Number(prompt("What is the value of c"));
 
 let perimeterTotal = aNum + bNum + cNum;
 console.log(perimeterTotal);
 
 //3.Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width));
-let lengthNum = Number(prompt('What is the value of the length'));
-let breadthNum = Number(prompt('What is the value of the length'));
+let lengthNum = Number(prompt("What is the value of the length"));
+let breadthNum = Number(prompt("What is the value of the length"));
 
 let areaTotal = lengthNum * breadthNum;
 let periTotal = 2 * (lengthNum + breadthNum);
-console.log(`Area of rectangle: ${areaTotal}`, `Perimeter of rectangle: ${periTotal}`);
+console.log(
+  `Area of rectangle: ${areaTotal}`,
+  `Perimeter of rectangle: ${periTotal}`
+);
 
 //4.Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
-let circleRadius = Number(prompt('What is the radius of the circle?'));
+let circleRadius = Number(prompt("What is the radius of the circle?"));
 
 let area = Math.PI * circleRadius * circleRadius;
 let circumferenceCircle = 2 * Math.PI * circleRadius;
-console.log(`Area of circle: ${area}`, `Circumference of circle: ${circumferenceCircle}`);
+console.log(
+  `Area of circle: ${area}`,
+  `Circumference of circle: ${circumferenceCircle}`
+);
 
 //5.Calculate the slope, x-intercept and y-intercept of y = 2x -2
-let xValue = Number(prompt('What is the value of x?'));
+let xValue = Number(prompt("What is the value of x?"));
 let slopeTotal = 2 * xValue - 2;
 
 console.log(slopeTotal);
@@ -130,34 +151,37 @@ let value_x_2 = 6;
 let value_y_1 = 2;
 let value_y_2 = 10;
 
-let totalSLope = (value_y_2 - value_y_1)/(value_x_2 - value_x_1);
+let totalSLope = (value_y_2 - value_y_1) / (value_x_2 - value_x_1);
 console.log(totalSLope);
 
 //7.
 
 //8.Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
 let value_x = 2;
-let value_y = (value_x*value_x + 6*value_x + 9);
+let value_y = value_x * value_x + 6 * value_x + 9;
 
-console.log(value_y)
+console.log(value_y);
 
 //9.Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
-let workHours  = Number(prompt("How many Hours do you work in a week?"));
-let rate = Number(prompt('What is the rate per hour?'));
+let workHours = Number(prompt("How many Hours do you work in a week?"));
+let rate = Number(prompt("What is the rate per hour?"));
 
 let weeklyWage = workHours * rate;
 console.log(`Your weekly wage is: ${weeklyWage}.`);
 
 //10. If the length of your name is greater than 7 say, your name is long else say your name is short.
-let yourName = prompt('What is your name?');
+let yourName = prompt("What is your name?");
 
 result = yourName.length > 7 ? "Your name is long." : "Your name is short";
 console.log(result);
 
 //11.Compare your first name length and your family name length and you should get this output.
 
-endResult = firstName.length > lastName.length ? `Your first name is longer than your last name` : "Your last name is longer than your first"
-console.log(endResult)
+endResult =
+  firstName.length > lastName.length
+    ? `Your first name is longer than your last name`
+    : "Your last name is longer than your first";
+console.log(endResult);
 
 //12.Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
 my_age = 24;
@@ -171,26 +195,40 @@ let birthYear = Number(prompt("Enter birth year?"));
 userAge = birthYear - dateNow.getFullYear();
 let difference = 18 - userAge;
 
-let outcome = userAge >= 18 ? `You are ${userAge}. You are old enough to drive` : `You are ${userAge}.You will be allowed to drive ${difference} years after`;
+let outcome =
+  userAge >= 18
+    ? `You are ${userAge}. You are old enough to drive`
+    : `You are ${userAge}.You will be allowed to drive ${difference} years after`;
 console.log(outcome);
 
 //14.Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years.
-let yearsLived = Number(prompt('Enter number of years you live:'));
+let yearsLived = Number(prompt("Enter number of years you live:"));
 let secondsLived = yearsLived * 31536000;
 
 console.log(secondsLived);
 
-
 //15.Create a human readable time format using the Date time object
 //i.YYYY-MM-DD HH:mm
 let newDate = new Date();
-console.log(`${newDate.getFullYear()}-0${newDate.getMonth() + 1}-${newDate.getDate()} ${newDate.getHours()}:${newDate.getMinutes()}`);
+console.log(
+  `${newDate.getFullYear()}-0${
+    newDate.getMonth() + 1
+  }-${newDate.getDate()} ${newDate.getHours()}:${newDate.getMinutes()}`
+);
 
 //ii. DD-MM-YYYY HH:mm
-console.log(`${newDate.getDate()}-0${newDate.getMonth() + 1}-${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}`);
+console.log(
+  `${newDate.getDate()}-0${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}`
+);
 
 //iii.DD/MM/YYYY HH:mm
-console.log(`${newDate.getDate()}/0${newDate.getMonth() + 1}/${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}`);
+console.log(
+  `${newDate.getDate()}/0${
+    newDate.getMonth() + 1
+  }/${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}`
+);
 
 //Exercise Level 3;
 //1.Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
@@ -206,10 +244,6 @@ hoursResult = hours < 10 ? `0${hours}` : hours;
 monthsResult = months < 10 ? `0${months}` : months;
 daysResult = days < 10 ? `0${days}` : days;
 
-console.log(`${years}-${monthsResult}-${daysResult} ${hoursResult}:${minutesResult}`);
-
-
-
-
-
-
+console.log(
+  `${years}-${monthsResult}-${daysResult} ${hoursResult}:${minutesResult}`
+);
