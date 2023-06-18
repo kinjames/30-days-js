19844aaea001e382f75bcf4de5bbf2321a9f36d1
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-06-18 20:29:22 +0000

    update

--- day23/script.js
+++ day23/script.js
@@ -4,6 +4,13 @@ console.log("Hello,World!!");
 // 1.Generating numbers and marking evens, odds and prime numbers with three different colors. See the image below.
 
 let userInput = document.querySelector(".input").value;
+let btn = document.querySelector(".btn");
+
+let form = document.querySelector(".for");
+
+btn.addEventListener("submit", () => {
+  console.log(userInput);
+});
 
 console.log(userInput);
 
