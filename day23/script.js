97bc203d8bbaa9f2a8128ec8a185b70bdf0dd054
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-06-18 20:21:44 +0000

    update

--- day23/script.js
+++ day23/script.js
@@ -2,3 +2,7 @@
 console.log("Hello,World!!");
 // Exercise Level One
 // 1.Generating numbers and marking evens, odds and prime numbers with three different colors. See the image below.
+
+let userInput = document.querySelector(".input").value;
+
+// for(i = 0, i <)
